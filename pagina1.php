bc6850ff6dac6d34e8ed89dee711a1555c2a7d5a
Juan Carlos Perez Solis <juancarloscars123@gmail.com>
2017-08-22 18:09:20 -0500

    se creo la primera pagina HTML agregando comando php para intoducir 5 comentarios

---
+++ pagina1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8">
+
+<title>juan carlos perez solis</title>
+
+</head>
+
+<body>
+<h1>Mi pagina en PHP</h1>
+<?PHP
+   $variable1=1;
+echo $variable1;
+echo"<br>";
+echo "<p>Esta es una pagina en php realizada en clase</p>";
+echo"<br>";
+echo "<p>Mensaje mensaje mensaje</p>";
+echo"<br>";
+echo "<p>Juan Carlos Perez Solis</p>";
+echo"<br>";
+echo "<p>Esta es una pagina en php realizada en clase</p>";
+echo"<br>";
+echo "<p>Hola </p>";
+?>
+</body>
+
+</html>
